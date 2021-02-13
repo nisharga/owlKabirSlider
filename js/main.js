10113c50faa64851d47112f6d390d14c853b692e
Nisharga Kabir <34031225+nisharga@users.noreply.github.com>
2021-02-13 13:43:00 +0600

    bug update

--- js/main.js
+++ js/main.js
@@ -2,19 +2,12 @@ jQuery(document).ready(function($) {
      var owl = $('.slider-area');
         owl.owlCarousel({
                 items: 1,
-                nav: false,
+                nav: true,
                 dots: true,
                 loop: true, 
                 autoplay: true,
-        });
-     var owls = $('.logo-carousel');
-        owls.owlCarousel({
-                items: 5,
-                margin: 30,
-                nav: false,
-                dots: true,
-                loop: true, 
-                autoplay: false,
-        });
+                responsiveClass:true,
+                
+        }); 
 });      
      
